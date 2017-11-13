406249ae81b177ecfb4dbdd6bf71c31c0586d63b
Chris Violette <cvioletterun@gmail.com>
2017-11-13 11:34:41 -0500

    Adding actual tier costs

--- src/data/TierCostList.js
+++ src/data/TierCostList.js
@@ -1,42 +1,42 @@
 const TierCostList = {
   preventative: {
     1: {
-      'ppohome': 1,
-      'ppochoice': 2,
-      'hdhphome': 3,
+      'ppohome': 4,
+      'ppochoice': 4,
+      'hdhphome': 4,
       'hdhpchoice': 4
     },
     2: {
-      'ppohome': 5,
-      'ppochoice': 6,
-      'hdhphome': 7,
-      'hdhpchoice': 8
+      'ppohome': 20,
+      'ppochoice': 30,
+      'hdhphome': 20,
+      'hdhpchoice': 30
     },
     3: {
-      'ppohome': 9,
-      'ppochoice': 10,
-      'hdhphome': 11,
-      'hdhpchoice': 12
+      'ppohome': 40,
+      'ppochoice': 50,
+      'hdhphome': 40,
+      'hdhpchoice': 50
     }
   },
   nonpreventative: {
     1: {
-      'ppohome': 13,
-      'ppochoice': 14,
-      'hdhphome': 15,
-      'hdhpchoice': 16
+      'ppohome': 4,
+      'ppochoice': 4,
+      'hdhphome': 4,
+      'hdhpchoice': 4
     },
     2: {
-      'ppohome': 17,
-      'ppochoice': 18,
-      'hdhphome': 19,
-      'hdhpchoice': 20
+      'ppohome': 20,
+      'ppochoice': 30,
+      'hdhphome': 20,
+      'hdhpchoice': 30
     },
     3: {
-      'ppohome': 21,
-      'ppochoice': 22,
-      'hdhphome': 23,
-      'hdhpchoice': 24
+      'ppohome': 40,
+      'ppochoice': 50,
+      'hdhphome': 40,
+      'hdhpchoice': 50
     }
   }
 }
